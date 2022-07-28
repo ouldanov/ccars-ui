c1462492a881ffacf4ba77f1aebbb7d858202120
iuldanov <iuldanov@c-cars.tech>
2022-07-28 09:01:44 +0300

    fix lint

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,14 +1,23 @@
-import React, {ReactNode} from "react";
-import './Button.css'
+import React, { ReactNode } from 'react';
+import './Button.css';
 
 export type ButtonProps = {
-    onClick(): void
-    children: ReactNode
-    variant?: 'primary' | 'success'
-    disabled?: boolean
-}
+  onClick(): void;
+  children: ReactNode;
+  variant?: 'primary' | 'success';
+  disabled?: boolean;
+};
 
-export function Button({children, onClick, variant = 'primary', disabled}: ButtonProps) {
-    const className = `button ${variant}`;
-    return <button className={className} onClick={onClick} disabled={disabled}>{children}</button>
+export function Button({
+  children,
+  onClick,
+  variant = 'primary',
+  disabled,
+}: ButtonProps) {
+  const className = `button ${variant}`;
+  return (
+    <button className={className} onClick={onClick} disabled={disabled}>
+      {children}
+    </button>
+  );
 }
--- test/Button.test.tsx
+++ test/Button.test.tsx
@@ -1,41 +1,53 @@
-import * as React from 'react'
-import {fireEvent, render, screen} from "@testing-library/react";
-import {Button} from "../src";
+import * as React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Button } from '../src';
 
 describe('Button', () => {
-    describe('render', () => {
-        test('default', () => {
-            render(<Button onClick={console.log}>render default</Button>);
-            const button = screen.getByText('render default');
-            expect(button.classList.contains('primary')).toBeTruthy();
-        })
-        test('success', () => {
-            const { container } = render(<Button variant="success" onClick={console.log}>render success</Button>);
-            const button = container.querySelector('button');
-            expect(button!.classList.contains('success')).toBeTruthy();
-        })
-        test('disabled', () => {
-            const { asFragment } = render(<Button disabled={true} onClick={console.log}>render disabled</Button>);
-            expect(asFragment()).toMatchSnapshot(`
+  describe('render', () => {
+    test('default', () => {
+      render(<Button onClick={console.log}>render default</Button>);
+      const button = screen.getByText('render default');
+      expect(button.classList.contains('primary')).toBeTruthy();
+    });
+    test('success', () => {
+      const { container } = render(
+        <Button variant="success" onClick={console.log}>
+          render success
+        </Button>
+      );
+      const button = container.querySelector('button');
+      expect(button!.classList.contains('success')).toBeTruthy();
+    });
+    test('disabled', () => {
+      const { asFragment } = render(
+        <Button disabled={true} onClick={console.log}>
+          render disabled
+        </Button>
+      );
+      expect(asFragment()).toMatchSnapshot(`
               <DocumentFragment>
                 <button class="button primary" disabled>render disabled</button>
               </DocumentFragment>
             `);
-        })
-    })
+    });
+  });
 
-    describe('clickable', () => {
-        test('default', () => {
-            const onClick = jest.fn();
-            render(<Button onClick={onClick}>clickable default</Button>);
-            fireEvent.click(screen.getByText('clickable default'));
-            expect(onClick).toHaveBeenCalledTimes(1);
-        })
-        test('disabled', () => {
-            const onClick = jest.fn();
-            render(<Button disabled={true} onClick={onClick}>clickable disabled</Button>);
-            fireEvent.click(screen.getByText('clickable disabled'));
-            expect(onClick).toHaveBeenCalledTimes(0);
-        })
-    })
-})
+  describe('clickable', () => {
+    test('default', () => {
+      const onClick = jest.fn();
+      render(<Button onClick={onClick}>clickable default</Button>);
+      fireEvent.click(screen.getByText('clickable default'));
+      expect(onClick).toHaveBeenCalledTimes(1);
+    });
+    test('disabled', () => {
+      const onClick = jest.fn();
+      render(
+        <Button disabled={true} onClick={onClick}>
+          clickable disabled
+        </Button>
+      );
+      fireEvent.click(screen.getByText('clickable disabled'));
+      expect(onClick).toHaveBeenCalledTimes(0);
+    });
+  });
+});
